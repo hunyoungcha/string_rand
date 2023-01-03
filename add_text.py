cedb65c0ef0aa5ab97b28b2dad9dc280838e5ff3
hunyoungcha <chamgf5247@naver.com>
2023-01-03 13:07:10 +0900

    5

--- add_text.py
+++ add_text.py
@@ -13,7 +13,7 @@ g=[]
 user_dict={'name':'','obs':'','note':''}
 
 
-#hwp의 받아온 텍스트 중 필요한 데이터만 temp.txt에 저장
+#hwp의 받아온 텍스트를 name obs note의 프레임으로 만들어 temp.txt에 저장
 def add_temp():
     stop=0
     temp=open('temp.txt', 'w', encoding='utf-8')
@@ -33,13 +33,26 @@ def add_temp():
 
 #temp 에서 all_data로 기준에 맞는 값만 옮기는 함수
 #중간 중간 관찰내용이 아니라 이름이 들어가 있는 경우가 많음 ,고쳐야함
-def temp_to_all_data():
+def temp_data_cont():
     t=open('temp.txt','r', encoding='utf-8')
     data_list=t.readlines()
     t.close()
-
+    
+    tmp=[]
+    for i in data_list:
+        q=i.split('\n')[0]
+        if len(q)>3:
+            tmp.append(q)
+    
     ad=open('all_data.txt','w', encoding='utf-8')
-    for i in range(1,len(data_list),3):
-        ad.write(data_list[i])
+    for i in range(0,len(tmp),2):
+        ad.write(tmp[i]+'\n')
+    ad.close()
+  
 
-temp_to_all_data()
+
+    # ad=open('all_data.txt','w', encoding='utf-8')
+    # for i in range(1,len(data_list),3):
+    #     ad.write(data_list[i])
+
+temp_data_cont()
